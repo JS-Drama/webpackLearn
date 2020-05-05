07e846b16806e9b6e8ef94b84a36812e61cd8f65
e <AbandonedDrama@foxmail.com>
2020-05-06 00:08:15 +0800

    管理输出 webpack官网教程有问题 查看源码webpack.config.js 注释

--- src/index.js
+++ src/index.js
@@ -1,22 +1,18 @@
 import _ from 'lodash';
-import './style.css';
-import Icon from'./icon.jpg';
-import Data from './data.xml';
+import printMe from './print.js';
 
 function component() {
     var element = document.createElement('div');
+    var btn = document.createElement('button');
 
     // Lodash（目前通过一个 script 脚本引入）对于执行这一行是必需的
     element.innerHTML = _.join(['Hello', 'webpack'], ' ');
-    element.classList.add('hello');
 
-    // 将图像添加到我们已经存在的 div 中。
-    var myIcon = new Image();
-    myIcon.src = Icon;
+    btn.innerHTML = '点击这里，然后查看 console！';
+    btn.onclick = printMe;
 
-    element.appendChild(myIcon);
+    element.appendChild(btn);
 
-    console.log(Data);
     return element;
 }
 
