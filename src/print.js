a471d8ee62c826dc2516c0db44100e82beabc1b3
drama <drama@chtech8.net>
2020-05-07 14:21:57 +0800

    CHPC SSH TEST

--- src/print.js
+++ src/print.js
@@ -1,3 +1,12 @@
+/*
+ * @Author: JS-Drama
+ * @Date: 2020-05-07 14:20:42
+ * @LastEditTime: 2020-05-07 14:21:22
+ * @LastEditors: Please set LastEditors
+ * @Description: In User Settings Edit
+ * @FilePath: \pj-h5-webd:\myCode\webpackLearn\src\print.js
+ */
 export default function printMe() {
     console.log('I get called from print.js!');
+    // 测试连接
 }
