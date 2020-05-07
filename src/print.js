1616bc9ee52f8f66361784b0bc98188d757bef4d
drama <drama@chtech8.net>
2020-05-07 15:05:20 +0800

    CHPC GIT PUSH ERROR

--- src/print.js
+++ src/print.js
@@ -1,12 +1,12 @@
 /*
  * @Author: JS-Drama
  * @Date: 2020-05-07 14:20:42
- * @LastEditTime: 2020-05-07 14:21:22
+ * @LastEditTime: 2020-05-07 15:04:58
  * @LastEditors: Please set LastEditors
  * @Description: In User Settings Edit
  * @FilePath: \pj-h5-webd:\myCode\webpackLearn\src\print.js
  */
 export default function printMe() {
     console.log('I get called from print.js!');
-    // 测试连接
+    // 测试连接 再次尝试
 }
